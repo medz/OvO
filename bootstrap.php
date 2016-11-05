edd06466cde1524bc0ca7caf397f8eea582a16d4
Seven <lovevipdsw@outlook.com>
2016-11-05 15:39:35 +0800

    添加启动文件

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+$filename = __DIR__.'/vendor/autoload.php';
+if (!file_exists($filename) || !is_file($filename)) {
+    echo '<pre>',
+    echo 'You must set up the project dependencies, run the following commands:', PHP_EOL,
+         'curl -sS https://getcomposer.org/installer | php', PHP_EOL,
+         'php composer.phar install', PHP_EOL,
+    echo '</pre>';
+    exit;
+}
+
+require $filename;
