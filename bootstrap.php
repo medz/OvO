ea31d53ccc2ec21ec9e8a4c20237b35ee7bb7db4
Seven <lovevipdsw@outlook.com>
2016-11-05 15:48:32 +0800

    修复启动文件错误

--- bootstrap.php
+++ bootstrap.php
@@ -3,10 +3,10 @@
 $filename = __DIR__.'/vendor/autoload.php';
 if (!file_exists($filename) || !is_file($filename)) {
     echo '<pre>',
-    echo 'You must set up the project dependencies, run the following commands:', PHP_EOL,
+         'You must set up the project dependencies, run the following commands:', PHP_EOL,
          'curl -sS https://getcomposer.org/installer | php', PHP_EOL,
          'php composer.phar install', PHP_EOL,
-    echo '</pre>';
+         '</pre>';
     exit;
 }
 
