4ccdd83c3b3315388b64c1e5bdb5ebf77481abb1
Seven <lovevipdsw@outlook.com>
2016-11-08 21:54:44 +0800

    Applied fixes from StyleCI (#29)

--- src/applications/task/admin/TaskConditionBbsController.php
+++ src/applications/task/admin/TaskConditionBbsController.php
@@ -1,4 +1,5 @@
 <?php
+
 Wind::import('ADMIN:library.AdminBaseController');
 
 /**
@@ -10,79 +11,88 @@ Wind::import('ADMIN:library.AdminBaseController');
  * @version $Id: TaskConditionBbsController.php 20430 2012-10-29 10:46:33Z xiaoxia.xuxx $
  * @package src.modules.task.admin
  */
-class TaskConditionBbsController extends AdminBaseController {
-
-	/* (non-PHPdoc)
-	 * @see AdminBaseController::beforeAction()
-	 */
-	public function beforeAction($handlerAdapter) {
+class TaskConditionBbsController extends AdminBaseController
+{
+    /* (non-PHPdoc)
+     * @see AdminBaseController::beforeAction()
+     */
+    public function beforeAction($handlerAdapter)
+    {
         parent::beforeAction($handlerAdapter);
         $var = json_decode(urldecode($this->getInput('var', 'post')), true);
-		if (is_array($var)) {
-			$this->setOutput($var, 'condition');
-		}
-	}
-	
-	/**
-	 * 构建版块树
-	 *
-	 * @param int $parentid
-	 * @param array $map
-	 * @param int $level
-	 * @return array
-	 */
-	private function _buildForumTree($parentid, $map, $level = '') {
-		if (!isset($map[$parentid])) return array();
-		$array = array();
-		foreach ($map[$parentid] as $key => $value) {
-			$value['level'] = $level;
-			$value['name'] = strip_tags($value['name']);
-			$array[] = $value;
-			$array = array_merge($array, $this->_buildForumTree($value['fid'], $map, $level.'&nbsp;&nbsp;'));
-		}
-		return $array;
-	}
-	
-	/**
-	 * 获得版块列表
-	 *
-	 */
-	private function _getForumList() {
-		/* @var $forumSrv PwForumService */
-		$forumSrv = Wekit::load('forum.srv.PwForumService');
-		$map = $forumSrv->getForumMap();
-		$catedb = $map[0];
-		foreach ($catedb as $_k => $_v) {
-			$catedb[$_k]['name'] = strip_tags($_v['name']);
-		}
-		$forumList = array();
-		foreach ($catedb as $value) {
-			$forumList[$value['fid']] = $this->_buildForumTree($value['fid'], $map);
-		}
-		$this->setOutput($catedb, 'catedb');
-		$this->setOutput($forumList, 'forumList');
-	}
+        if (is_array($var)) {
+            $this->setOutput($var, 'condition');
+        }
+    }
 
-	/* (non-PHPdoc)
-	 * @see WindController::run()
-	 */
-	public function run() {
-		$this->_getForumList();
-		$this->setTemplate('condition.bbs_post');
-	}
+    /**
+     * 构建版块树
+     *
+     * @param  int   $parentid
+     * @param  array $map
+     * @param  int   $level
+     * @return array
+     */
+    private function _buildForumTree($parentid, $map, $level = '')
+    {
+        if (!isset($map[$parentid])) {
+            return array();
+        }
+        $array = array();
+        foreach ($map[$parentid] as $key => $value) {
+            $value['level'] = $level;
+            $value['name'] = strip_tags($value['name']);
+            $array[] = $value;
+            $array = array_merge($array, $this->_buildForumTree($value['fid'], $map, $level.'&nbsp;&nbsp;'));
+        }
 
-	/**
-	 * 回帖
-	 */
-	public function replyAction() {
-		$this->setTemplate('condition.bbs_reply');
-	}
+        return $array;
+    }
 
-	/**
-	 * 喜欢帖子
-	 */
-	public function likeAction() {
-		$this->_getForumList();
-		$this->setTemplate('condition.bbs_like');
-	}
+    /**
+     * 获得版块列表
+     *
+     */
+    private function _getForumList()
+    {
+        /* @var $forumSrv PwForumService */
+        $forumSrv = Wekit::load('forum.srv.PwForumService');
+        $map = $forumSrv->getForumMap();
+        $catedb = $map[0];
+        foreach ($catedb as $_k => $_v) {
+            $catedb[$_k]['name'] = strip_tags($_v['name']);
+        }
+        $forumList = array();
+        foreach ($catedb as $value) {
+            $forumList[$value['fid']] = $this->_buildForumTree($value['fid'], $map);
+        }
+        $this->setOutput($catedb, 'catedb');
+        $this->setOutput($forumList, 'forumList');
+    }
+
+    /* (non-PHPdoc)
+     * @see WindController::run()
+     */
+    public function run()
+    {
+        $this->_getForumList();
+        $this->setTemplate('condition.bbs_post');
+    }
+
+    /**
+     * 回帖
+     */
+    public function replyAction()
+    {
+        $this->setTemplate('condition.bbs_reply');
+    }
+
+    /**
+     * 喜欢帖子
+     */
+    public function likeAction()
+    {
+        $this->_getForumList();
+        $this->setTemplate('condition.bbs_like');
+    }
 }
--- src/applications/task/admin/TaskRewardController.php
+++ src/applications/task/admin/TaskRewardController.php
@@ -1,4 +1,5 @@
 <?php
+
 Wind::import('ADMIN:library.AdminBaseController');
 /**
  * 任务奖励扩展-设置端
@@ -9,40 +10,43 @@ Wind::import('ADMIN:library.AdminBaseController');
  * @version $Id: TaskRewardController.php 15745 2012-08-13 02:45:07Z xiaoxia.xuxx $
  * @package src.modules.task.admin
  */
-class TaskRewardController extends AdminBaseController {
+class TaskRewardController extends AdminBaseController
+{
+    /* (non-PHPdoc)
+     * @see AdminBaseController::beforeAction()
+     */
+    public function beforeAction($handlerAdapter)
+    {
+        parent::beforeAction($handlerAdapter);
+        $var = json_decode(urldecode($this->getInput('var', 'post')), true);
+        if (is_array($var)) {
+            $this->setOutput($var, 'reward');
+        }
+    }
 
-	/* (non-PHPdoc)
-	 * @see AdminBaseController::beforeAction()
-	 */
-	public function beforeAction($handlerAdapter) {
-		parent::beforeAction($handlerAdapter);
-		$var = json_decode(urldecode($this->getInput('var', 'post')), true);
-		if (is_array($var)) {
-			$this->setOutput($var, 'reward');
-		}
-	}
-	
-	/* (non-PHPdoc)
-	 * 任务奖励扩展-积分扩展
-	 * @see WindController::run()
-	 */
-	public function run() {
-		Wind::import('SRV:credit.bo.PwCreditBo');
-		/* @var $pwCreditBo PwCreditBo */
-		$pwCreditBo = PwCreditBo::getInstance();
-		
-		$this->setOutput($pwCreditBo, 'credit');
-		$this->setTemplate('reward.reward_credit');
-	}
-	
-	/**
-	 * 任务奖励扩展-用户组扩展
-	 */
-	public function groupAction() {
-		/* @var $userGroups PwUserGroups */
-		$userGroups = Wekit::load('usergroup.PwUserGroups');
-		$groupList = $userGroups->getGroupsByType('special');
-		$this->setOutput($groupList, 'groups');
-		$this->setTemplate('reward.reward_group');
-	}
+    /* (non-PHPdoc)
+     * 任务奖励扩展-积分扩展
+     * @see WindController::run()
+     */
+    public function run()
+    {
+        Wind::import('SRV:credit.bo.PwCreditBo');
+        /* @var $pwCreditBo PwCreditBo */
+        $pwCreditBo = PwCreditBo::getInstance();
+
+        $this->setOutput($pwCreditBo, 'credit');
+        $this->setTemplate('reward.reward_credit');
+    }
+
+    /**
+     * 任务奖励扩展-用户组扩展
+     */
+    public function groupAction()
+    {
+        /* @var $userGroups PwUserGroups */
+        $userGroups = Wekit::load('usergroup.PwUserGroups');
+        $groupList = $userGroups->getGroupsByType('special');
+        $this->setOutput($groupList, 'groups');
+        $this->setTemplate('reward.reward_group');
+    }
 }
