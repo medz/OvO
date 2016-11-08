d27b604d44ce3c65d5cca0bd3cbb28c6b2b59f0d
Seven <lovevipdsw@outlook.com>
2016-11-08 21:54:16 +0800

    GET型CSRF任意代码执行

--- src/applications/task/admin/TaskConditionMemberController.php
+++ src/applications/task/admin/TaskConditionMemberController.php
@@ -1,5 +1,4 @@
 <?php
-
 Wind::import('ADMIN:library.AdminBaseController');
 
 /**
@@ -11,57 +10,51 @@ Wind::import('ADMIN:library.AdminBaseController');
  * @version $Id: TaskConditionMemberController.php 15745 2012-08-13 02:45:07Z xiaoxia.xuxx $
  * @package src.modules.task.admin
  */
-class TaskConditionMemberController extends AdminBaseController
-{
-    /* (non-PHPdoc)
-     * @see AdminBaseController::beforeAction()
-     */
-    public function beforeAction($handlerAdapter)
-    {
-        parent::beforeAction($handlerAdapter);
-        $var = unserialize($this->getInput('var'));
-        if (is_array($var)) {
-            $this->setOutput($var, 'condition');
-        }
-    }
-
-    /**
-     * 完成资料
-     */
-    public function profileAction()
-    {
-        $this->setTemplate('condition.member_profile');
-    }
-
-    /**
-     * 上传头像
-     */
-    public function avatarAction()
-    {
-        $this->setTemplate('condition.member_avatar');
-    }
-
-    /**
-     * 发送消息
-     */
-    public function sendMsgAction()
-    {
-        $this->setTemplate('condition.member_msg');
-    }
-
-    /**
-     * 打卡
-     */
-    public function punchAction()
-    {
-        $this->setTemplate('condition.member_punch');
-    }
-
-    /**
-     * 求粉丝
-     */
-    public function fansAction()
-    {
-        $this->setTemplate('condition.member_fans');
-    }
+class TaskConditionMemberController extends AdminBaseController{
+	
+	/* (non-PHPdoc)
+	 * @see AdminBaseController::beforeAction()
+	 */
+	public function beforeAction($handlerAdapter) {
+		parent::beforeAction($handlerAdapter);
+		$var = json_decode(urldecode($this->getInput('var', 'post')), true);
+		if (is_array($var)) {
+			$this->setOutput($var, 'condition');
+		}
+	}
+	
+	/**
+	 * 完成资料
+	 */
+	public function profileAction() {
+		$this->setTemplate('condition.member_profile');
+	}
+	
+	/**
+	 * 上传头像
+	 */
+	public function avatarAction() {
+		$this->setTemplate('condition.member_avatar');
+	}
+	
+	/**
+	 * 发送消息
+	 */
+	public function sendMsgAction() {
+		$this->setTemplate('condition.member_msg');
+	}
+	
+	/**
+	 * 打卡
+	 */
+	public function punchAction() {
+		$this->setTemplate('condition.member_punch');
+	}
+	
+	/**
+	 * 求粉丝
+	 */
+	public function fansAction() {
+		$this->setTemplate('condition.member_fans');
+	}
 }
