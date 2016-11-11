88f09fd41bcbc62b1e8156301ed077b2a998d78d
Seven <lovevipdsw@outlook.com>
2016-11-11 22:35:00 +0800

    Applied fixes from StyleCI (#39)

--- src/extensions/search/service/PwForum.php
+++ src/extensions/search/service/PwForum.php
@@ -1,4 +1,5 @@
 <?php
+
 defined('WEKIT_VERSION') || exit('Forbidden');
 
 /**
@@ -11,23 +12,26 @@ defined('WEKIT_VERSION') || exit('Forbidden');
  * @package forum
  */
 
-class PwForum {
-	
-	const FETCH_MAIN = 1;		//版块主要信息
-	const FETCH_STATISTICS = 2;	//版块统计信息
-	const FETCH_EXTRA = 4;		//版块扩展信息
-	const FETCH_ALL = 7;
-	
-	
-	public function searchDesignForum(PwForumSo $so, $limit = 20, $offset = 0) {
-		return $this->_getDesignForumDao()->searchForum($so->getData(), $so->getOrderby(), $limit, $offset);
-	}
-	
-	public function countSearchForum(PwForumSo $so) {
-		return $this->_getDesignForumDao()->countSearchForum($so->getData());
-	}
+class PwForum
+{
+    const FETCH_MAIN = 1;        //版块主要信息
+    const FETCH_STATISTICS = 2;    //版块统计信息
+    const FETCH_EXTRA = 4;        //版块扩展信息
+    const FETCH_ALL = 7;
 
-	protected function _getDesignForumDao(){
-		return Wekit::loadDao('EXT:search.service.dao.PwDesignForumDao');
-	}
+
+    public function searchDesignForum(PwForumSo $so, $limit = 20, $offset = 0)
+    {
+        return $this->_getDesignForumDao()->searchForum($so->getData(), $so->getOrderby(), $limit, $offset);
+    }
+
+    public function countSearchForum(PwForumSo $so)
+    {
+        return $this->_getDesignForumDao()->countSearchForum($so->getData());
+    }
+
+    protected function _getDesignForumDao()
+    {
+        return Wekit::loadDao('EXT:search.service.dao.PwDesignForumDao');
+    }
 }
