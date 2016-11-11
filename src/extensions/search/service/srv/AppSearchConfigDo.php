7b4da7185fb8cfa210b8a4b55b3eb5f0477ed824
Seven <lovevipdsw@outlook.com>
2016-11-11 22:37:51 +0800

    Applied fixes from StyleCI (#40)

--- src/extensions/search/service/srv/AppSearchConfigDo.php
+++ src/extensions/search/service/srv/AppSearchConfigDo.php
@@ -1,52 +1,53 @@
 <?php
-class AppSearchConfigDo {
-	
-	/**
-	 * 获取搜索用户组权限配置
-	 *
-	 * @param unknown_type $config
-	 * @return multitype:multitype:string  multitype:string boolean  
-	 */
-	public function getPermissionConfig($config)
-	{
-		$config += array(
-			'app_search_open' => array('radio', 'basic', '易搜索', ''),
-			'app_search_time_interval' => array('input', 'basic', '易搜索间隔', '请输入用户两次搜索的时间间隔', ''),
-		);
-		return $config;
-	}
-	
-	/**
-	 * 获取搜索用户组根权限配置
-	 *
-	 * @param array $config
-	 * @return multitype:multitype:string  
-	 */
-	public function getPermissionCategoryConfig($config)
-	{
-		$searchconfig = array(
-			'other' => array(
-				'sub' => array(
-					'search' => array(
-						'name' => '易搜索',
-						'items' => array(
-							'app_search_open','app_search_time_interval'
-						),
-					),
-				),
-			),
-		);
-		return WindUtility::mergeArray($config,$searchconfig);
-	}
-	
-	public function getAdminMenu($config)
-	{
-		$config += array(
-			'app_search' => array('易搜索', 'app/manage/*?app=search', '', '', 'appcenter'),
-			);
-		return $config;
-	}
-	
-}
 
-?>
+class AppSearchConfigDo
+{
+    /**
+     * 获取搜索用户组权限配置
+     *
+     * @param  unknown_type               $config
+     * @return multitype:multitype:string multitype:string boolean
+     */
+    public function getPermissionConfig($config)
+    {
+        $config += array(
+            'app_search_open' => array('radio', 'basic', '易搜索', ''),
+            'app_search_time_interval' => array('input', 'basic', '易搜索间隔', '请输入用户两次搜索的时间间隔', ''),
+        );
+
+        return $config;
+    }
+
+    /**
+     * 获取搜索用户组根权限配置
+     *
+     * @param  array                      $config
+     * @return multitype:multitype:string
+     */
+    public function getPermissionCategoryConfig($config)
+    {
+        $searchconfig = array(
+            'other' => array(
+                'sub' => array(
+                    'search' => array(
+                        'name' => '易搜索',
+                        'items' => array(
+                            'app_search_open', 'app_search_time_interval',
+                        ),
+                    ),
+                ),
+            ),
+        );
+
+        return WindUtility::mergeArray($config, $searchconfig);
+    }
+
+    public function getAdminMenu($config)
+    {
+        $config += array(
+            'app_search' => array('易搜索', 'app/manage/*?app=search', '', '', 'appcenter'),
+            );
+
+        return $config;
+    }
+}
--- src/extensions/search/service/srv/action/App_SearchThread.php
+++ src/extensions/search/service/srv/action/App_SearchThread.php
@@ -1,67 +1,69 @@
-﻿<?php
+<?php
+
 Wind::import('EXT:search.service.srv.action.App_SearchAction');
 
 class App_SearchThread extends App_SearchAction
 {
-	protected $fid = 0;
-	
-	/**
-	 * 搜索统计帖子
-	 * 
-	 * @param PwThreadSo $so
-	 * @return int
-	 */
-	function countSearch($so)
-	{
-		return $this->_getThreadSearch()->countSearchThread($so);
-	}
-	
-	/**
-	 * 搜索帖子
-	 * 
-	 * @param PwThreadSo $so
-	 * @param int $limit 查询条数
-	 * @param int $start 开始查询的位置
-	 * @return array
-	 */
-	function search($so, $limit = 20, $start = 0)
-	{
-		return $this->_getThreadSearch()->searchThread($so, $limit, $start, PwThread::FETCH_ALL);
-	}
+    protected $fid = 0;
 
-	/**
-	 * 组装帖子数据
-	 * @param array $threads
-	 * @param string $keywords
-	 * @return unknown_type
-	 */
-	public function build($list, $keywords)
-	{
-		if (!$list) return false;
-		$keywords = (is_array($keywords)) ? $keywords : explode(" ", $keywords);
-		$data = array();
-		foreach ($list as $t)
-		{
-			$t['subject'] = strip_tags($t['subject']);
-			$t['content'] = strip_tags($t['content']);
-			$t['content'] = Wekit::load('forum.srv.PwThreadService')->displayContent($t['content'], $t['useubb'], array(), 170);
-			foreach ($keywords as $keyword)
-			{
-				$keyword = stripslashes($keyword);
-				$keyword && $t['subject'] = $this->_highlighting($t['subject'], $keyword);
-				$keyword && $t['content'] = $this->_highlighting($t['content'], $keyword);
-			}
-			$data[] = $t;
-		}
-		return $data;
-	}
-	
-	/**
-	 * PwThread
-	 * @return PwThread
-	 */
-	protected function _getThreadSearch()
-	{
-		return Wekit::load('EXT:search.service.PwThread');
-	}
+    /**
+     * 搜索统计帖子
+     *
+     * @param  PwThreadSo $so
+     * @return int
+     */
+    public function countSearch($so)
+    {
+        return $this->_getThreadSearch()->countSearchThread($so);
+    }
+
+    /**
+     * 搜索帖子
+     *
+     * @param  PwThreadSo $so
+     * @param  int        $limit 查询条数
+     * @param  int        $start 开始查询的位置
+     * @return array
+     */
+    public function search($so, $limit = 20, $start = 0)
+    {
+        return $this->_getThreadSearch()->searchThread($so, $limit, $start, PwThread::FETCH_ALL);
+    }
+
+    /**
+     * 组装帖子数据
+     * @param  array        $threads
+     * @param  string       $keywords
+     * @return unknown_type
+     */
+    public function build($list, $keywords)
+    {
+        if (!$list) {
+            return false;
+        }
+        $keywords = (is_array($keywords)) ? $keywords : explode(' ', $keywords);
+        $data = array();
+        foreach ($list as $t) {
+            $t['subject'] = strip_tags($t['subject']);
+            $t['content'] = strip_tags($t['content']);
+            $t['content'] = Wekit::load('forum.srv.PwThreadService')->displayContent($t['content'], $t['useubb'], array(), 170);
+            foreach ($keywords as $keyword) {
+                $keyword = stripslashes($keyword);
+                $keyword && $t['subject'] = $this->_highlighting($t['subject'], $keyword);
+                $keyword && $t['content'] = $this->_highlighting($t['content'], $keyword);
+            }
+            $data[] = $t;
+        }
+
+        return $data;
+    }
+
+    /**
+     * PwThread
+     * @return PwThread
+     */
+    protected function _getThreadSearch()
+    {
+        return Wekit::load('EXT:search.service.PwThread');
+    }
 }
