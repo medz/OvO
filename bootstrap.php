b169d6357a980edaa7c88ce6ac11d3fdaac80bdc
Seven <lovevipdsw@outlook.com>
2016-11-14 16:34:00 +0800

    尝试性代码，兼容部分主机没有加载路径设置导致绝对路径使用报错的情况

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,25 @@
 <?php
 
+// 尝试性代码，兼容部分主机没有加载路径设置导致绝对路径使用报错的情况
+if (function_exists('get_include_path') && function_exists('set_include_path')) {
+    // 获取系统目前允许的路径
+    $includePaths = (array) explode(PATH_SEPARATOR, get_include_path());
+    $rootDir = __DIR__;
+
+    // 已经设置
+    foreach ($includePaths as $key => $value) {
+        if ($value == $root) {
+            unset($includePaths[$key]);
+        }
+    }
+
+    // 加入root的完整路径
+    array_push($includePaths, $rootDir);
+
+    // 设置include_path
+    @set_include_path(implode(PATH_SEPARATOR, $includePaths));
+}
+
 // 开发框架信息
 $frameworkAutoloadFile = dirname(__FILE__).'/windframework/vendor/autoload.php';
 if (file_exists($frameworkAutoloadFile) && is_file($frameworkAutoloadFile)) {
