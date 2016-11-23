551aaa66f4d56151802265404ccf910226bf55f2
Seven <shiweidu@outlook.com>
2016-11-23 22:54:59 +0800

    更新版本号～

--- src/library/Pw.php
+++ src/library/Pw.php
@@ -11,8 +11,8 @@
  */
 class Pw
 {
-    const VERSION = '1.0.2';
-    const VERSION_NAME = 'phpwind Fans 1.0.2';
+    const VERSION = '1.0.5';
+    const VERSION_NAME = 'phpwind Fans 1.0.5';
 
     /**
      * 取得指定名称的cookie值
