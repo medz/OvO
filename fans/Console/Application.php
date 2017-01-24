33361d183a283fce5ae48618b94cc7957e4866ea
Seven <shiweidu@outlook.com>
2017-01-24 15:15:09 +0800

    add fans version console.

---
+++ fans/Console/Application.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Pw\Console;
+
+use Symfony\Component\Console\Application as BaseApplication;
+
+class Application extends BaseApplication
+{
+    public function __construct()
+    {
+        parent::__construct('phpwind Fans console.', '1.0.0-dev');
+    }
+
+    /**
+     * Gets the default commands that should always be available.
+     *
+     * @return Command[] An array of default Command instances
+     */
+    protected function getDefaultCommands()
+    {
+        return array_merge(parent::getDefaultCommands(), array(
+
+        ));
+    }
+}
