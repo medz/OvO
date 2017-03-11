7f2539ae033e812ac7a997da4d23cc23c6cd1d0a
Seven <shiweidu@outlook.com>
2017-03-11 16:08:58 +0800

    删除调试代码～

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 error_reporting(E_ERROR | E_PARSE);
-ini_set('display_errors', true);
-error_reporting(E_ALL);
-define('WIND_DEBUG', 3);
+// ini_set('display_errors', true);
+// error_reporting(E_ALL);
+// define('WIND_DEBUG', 3);
 
 /*
 |--------------------------------------------------------------------------
