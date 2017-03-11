d79733b90fdf6579aa5415fcfd6c14dbb142ef16
Seven <shiweidu@outlook.com>
2017-03-11 15:52:48 +0800

    更新windid管理～

--- windid/admin.php
+++ windid/admin.php
@@ -1,7 +1,35 @@
 <?php
 
 error_reporting(E_ERROR | E_PARSE);
+// ini_set('display_errors', true);
+// error_reporting(E_ALL);
+// define('WIND_DEBUG', 3);
 
-require dirname(__DIR__).'/bootstrap.php';
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
 
-Wekit::run('windidadmin', array('router' => array()));
+$app = require dirname(__DIR__).'/bootstrap.php';
+
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$phpwind9 = $app->make('phpwind9');
+$phpwind9('windidadmin', ['router' => []]);
