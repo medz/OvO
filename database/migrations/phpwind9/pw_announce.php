3f59789fbfa8c4ca56419079a7f4402ac9b43a4c
Seven <shiweidu@outlook.com>
2017-03-29 21:34:22 +0800

    修复迁移

--- database/migrations/phpwind9/pw_announce.php
+++ database/migrations/phpwind9/pw_announce.php
@@ -22,37 +22,35 @@ class PwAnnounce extends Migration
             `created_userid` int(10) unsigned NULL DEFAULT '0',
             `typeid` tinyint(1) NULL DEFAULT '0',
             `url` varchar(80) DEFAULT '',
-              `subject` varchar(100) NULL DEFAULT '',
-              `content` MEDIUMTEXT,
-              `start_date` int(10) unsigned NULL DEFAULT '0',
-               `end_date` int(10) unsigned NULL DEFAULT '0',
+            `subject` varchar(100) NULL DEFAULT '',
+            `content` MEDIUMTEXT,
+            `start_date` int(10) unsigned NULL DEFAULT '0',
+            `end_date` int(10) unsigned NULL DEFAULT '0',
 
-              PRIMARY
-              KEY (`aid`),
-              KEY `idx_startdate` (`start_date`),
-              KEY `idx_vieworder` (`vieworder`)
-              ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='公告管理表';
-
-
+            PRIMARY KEY (`aid`),
+            KEY `idx_startdate` (`start_date`),
+            KEY `idx_vieworder` (`vieworder`)
+            ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='公告管理表';
          */
-                Schema::create('pw_announce', function (Blueprint $table) {
-                    if (env('DB_CONNECTION', false) === 'mysql') {
-                        $table->engine = 'InnoDB';
-                    }
+        Schema::create('pw_announce', function (Blueprint $table) {
+            if (env('DB_CONNECTION', false) === 'mysql') {
+                $table->engine = 'InnoDB';
+            }
 
-                    $table->smallInteger('aid')->
-            $table->smallInteger('vieworder')->nullable()->default('');
-                    $table->integer('created_userid')->unsigned()->default('');
-                    $table->tinyInteger('typeid')->nullable()->default('');
-                    $table->string('url', 80)->default('');
-                    $table->string('subject', 100)->nullable()->default('');
-                    $table->mediumText('content');
-                    $table->integer('start_date')->unsigned()->nullable()->default('');
-                    $table->integer('end_date')->unsigned()->nullable()->default('');
-                    $table->primary('aid');
-                    $table->index('staer_date');
-                    $table->index('vieworder');
-                });
+            $table->increments('aid');
+            $table->smallInteger('vieworder')->nullable()->default(0);
+            $table->integer('created_userid')->unsigned()->default(0);
+            $table->tinyInteger('typeid')->nullable()->default(0);
+            $table->string('url', 80)->default('');
+            $table->string('subject', 100)->nullable()->default('');
+            $table->mediumText('content');
+            $table->integer('start_date')->unsigned()->nullable()->default(0);
+            $table->integer('end_date')->unsigned()->nullable()->default(0);
+
+            $table->primary('aid');
+            $table->index('staer_date');
+            $table->index('vieworder');
+        });
     }
 
     /**
