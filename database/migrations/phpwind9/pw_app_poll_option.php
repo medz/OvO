3b90c970becaccdb8c3b9ec69b9da91eeaa1a542
Seven <shiweidu@outlook.com>
2017-04-06 02:07:21 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- database/migrations/phpwind9/pw_app_poll_option.php
+++ database/migrations/phpwind9/pw_app_poll_option.php
@@ -15,16 +15,16 @@ class PwAppPollOption extends Migration
     public function run()
     {
         /*
-			DROP TABLE IF EXISTS `pw_app_poll_option`;
-			CREATE TABLE `pw_app_poll_option` (
-			  `option_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '选项自增长ID',
-			  `poll_id` int(10) unsigned NULL DEFAULT '0' COMMENT '投票ID',
-			  `voted_num` mediumint(8) unsigned NULL DEFAULT '0' COMMENT '该选项投票数',
-			  `content` varchar(255) NULL DEFAULT '' COMMENT '选项内容',
-			  `image` varchar(255) NULL DEFAULT '' COMMENT '选项图片',
-			  PRIMARY KEY (`option_id`),
-			  KEY `idx_pollid` (`poll_id`)
-			) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='投票选项表';
+            DROP TABLE IF EXISTS `pw_app_poll_option`;
+            CREATE TABLE `pw_app_poll_option` (
+              `option_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '选项自增长ID',
+              `poll_id` int(10) unsigned NULL DEFAULT '0' COMMENT '投票ID',
+              `voted_num` mediumint(8) unsigned NULL DEFAULT '0' COMMENT '该选项投票数',
+              `content` varchar(255) NULL DEFAULT '' COMMENT '选项内容',
+              `image` varchar(255) NULL DEFAULT '' COMMENT '选项图片',
+              PRIMARY KEY (`option_id`),
+              KEY `idx_pollid` (`poll_id`)
+            ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='投票选项表';
          */
         Schema::create('pw_app_poll_option', function (Blueprint $table) {
             if (env('DB_CONNECTION', false) === 'mysql') {
