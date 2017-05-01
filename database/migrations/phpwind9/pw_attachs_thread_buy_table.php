b75be66bd62742c9f777c466b0beef118067b3bb
Seven <shiweidu@outlook.com>
2017-05-01 20:51:21 +0800

    添加 pw_attachs_thread_buy 迁移 #132

--- database/migrations/phpwind9/pw_attachs_thread_buy_table.php
+++ database/migrations/phpwind9/pw_attachs_thread_buy_table.php
@@ -33,7 +33,17 @@ class PwAttachsThreadBuyTable extends Migration
             if (env('DB_CONNECTION', false) === 'mysql') {
                 $table->engine = 'InnoDB';
             }
-        }
+
+            $table->increments('id');
+            $table->integer('aid')->unsigned()->nullable()->default(0);
+            $table->integer('created_userid')->unsigned()->nullable()->default(0);
+            $table->integer('created_time')->unsigned()->nullable()->default(0);
+            $table->mediumInteger('cost')->unsigned()->nullable()->default(0);
+            $table->tinyInteger('ctype')->unsigned()->nullable()->default(0);
+
+            $table->primary('id');
+            $table->index('aid');
+        };
     }
 
     /**
