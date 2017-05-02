dc7e3a02b5d121f1371f80bbbbd531f1a5fc5c87
beautifultable <beautifultable@gmail.com>
2017-05-02 17:49:01 +0800

    添加 pw_frag_template_table 迁移 #132

--- database/migrations/phpwind9/pw_frag_template_table.php
+++ database/migrations/phpwind9/pw_frag_template_table.php
@@ -30,6 +30,14 @@ class PwFragTemplateTable extends Migration
             if (env('DB_CONNECTION', false) === 'mysql') {
                 $table->engine = 'InnoDB';
             }
+			
+			$table->integer('tpl_id')->unsigned();
+			$table->smallInteger('frag_cid')->unsigned()->nullable()->default(0);
+			$table->string('tpl_name', 50)->nullable()->default('');
+			$table->text('template');
+			
+			$table->primary('tpl_id');
+			
         }
     }
 
