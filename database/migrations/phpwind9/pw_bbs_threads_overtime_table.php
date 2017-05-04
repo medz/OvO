404c1d7badfbafc9cb11840daf245446b067b2b4
Seven <shiweidu@outlook.com>
2017-05-04 02:05:05 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- database/migrations/phpwind9/pw_bbs_threads_overtime_table.php
+++ database/migrations/phpwind9/pw_bbs_threads_overtime_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 /*
 
@@ -18,32 +18,35 @@ UNIQUE KEY `idx_tid_mtype` (`tid`,`m_type`)
 
  */
 
-class PwBbsThreadsOvertimeTable extends Migration {
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function run() {
-		Schema::create('pw_bbs_threads_overtime', function (Blueprint $table) {
-			if (env('DB_CONNECTION', false) === 'mysql') {
-				$table->engine = 'InnoDB';
-			}
-			$table->increments('id')->unsigned();
-			$table->integer('tid')->unsigned()->nullable()->default(0);
-			$table->m_type('m_type', ['topped', 'highlight'])->nullable();
-			$table->integer('overtime')->unsigned()->nullable()->default(0);
-			$table->primary('id');
-			$table->unique(['tid', 'm_type']);
-		});
-	}
+class PwBbsThreadsOvertimeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schema::create('pw_bbs_threads_overtime', function (Blueprint $table) {
+            if (env('DB_CONNECTION', false) === 'mysql') {
+                $table->engine = 'InnoDB';
+            }
+            $table->increments('id')->unsigned();
+            $table->integer('tid')->unsigned()->nullable()->default(0);
+            $table->m_type('m_type', ['topped', 'highlight'])->nullable();
+            $table->integer('overtime')->unsigned()->nullable()->default(0);
+            $table->primary('id');
+            $table->unique(['tid', 'm_type']);
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down() {
-		Schema::dropIfExists('pw_bbs_threads_overtime');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pw_bbs_threads_overtime');
+    }
 }
