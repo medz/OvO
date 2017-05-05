7531496633f9dd42c99cc66725f14018a368574f
Seven <shiweidu@outlook.com>
2017-05-05 01:36:30 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- database/migrations/phpwind9/pw_design_push_table.php
+++ database/migrations/phpwind9/pw_design_push_table.php
@@ -45,26 +45,26 @@ class PwDesignPushTable extends Migration
             if (env('DB_CONNECTION', false) === 'mysql') {
                 $table->engine = 'InnoDB';
             }
-        $table->increments('push_id')->unsigned()->comment('推送ID');
-        $table->integer('push_from_id')->unsigned()->nullable()->default(0)->comment('来源ID');
-        $table->string('push_from_model', 20)->nullable()->default('')->comment('来源应用');
-        $table->integer('module_id')->unsigned()->nullable()->default(0)->comment('所属模块ID');
-        $table->string('push_standard', 255)->nullable()->default('')->comment('标准化标签');
-        $table->string('push_style', 255)->default('')->comment('样式');
-        $table->integer('push_orderid')->unsigned()->nullable()->default(0)->comment('排序');
-        $table->text('push_extend')->comment('推送内容');
-        $table->integer('created_userid')->unsigned()->nullable()->default(0)->comment('推送人ID');
-        $table->integer('author_uid')->unsigned()->nullable()->default(0)->comment('作者UID');
-        $table->tinyInteger('status')->unsigned()->nullable()->default(0)->comment('状态显示：0 需要审核：1');
-        $table->tinyInteger('neednotice')->unsigned()->nullable()->default(0)->comment('是否需要发送站内信');
-        $table->integer('check_uid')->unsigned()->nullable()->default(0)->comment('审核人ID');
-        $table->integer('created_time')->unsigned()->nullable()->default(0)->comment('推送时间');
-        $table->integer('start_time')->unsigned()->nullable()->default(0)->comment('开始时间');
-        $table->integer('end_time')->unsigned()->nullable()->default(0)->comment('过期时间');
-        $table->integer('checked_time')->unsigned()->nullable()->default(0)->comment('审核时间');
-        $table->primary('push_id');
-        $table->index('end_time');
-        $table->index('status');
+            $table->increments('push_id')->unsigned()->comment('推送ID');
+            $table->integer('push_from_id')->unsigned()->nullable()->default(0)->comment('来源ID');
+            $table->string('push_from_model', 20)->nullable()->default('')->comment('来源应用');
+            $table->integer('module_id')->unsigned()->nullable()->default(0)->comment('所属模块ID');
+            $table->string('push_standard', 255)->nullable()->default('')->comment('标准化标签');
+            $table->string('push_style', 255)->default('')->comment('样式');
+            $table->integer('push_orderid')->unsigned()->nullable()->default(0)->comment('排序');
+            $table->text('push_extend')->comment('推送内容');
+            $table->integer('created_userid')->unsigned()->nullable()->default(0)->comment('推送人ID');
+            $table->integer('author_uid')->unsigned()->nullable()->default(0)->comment('作者UID');
+            $table->tinyInteger('status')->unsigned()->nullable()->default(0)->comment('状态显示：0 需要审核：1');
+            $table->tinyInteger('neednotice')->unsigned()->nullable()->default(0)->comment('是否需要发送站内信');
+            $table->integer('check_uid')->unsigned()->nullable()->default(0)->comment('审核人ID');
+            $table->integer('created_time')->unsigned()->nullable()->default(0)->comment('推送时间');
+            $table->integer('start_time')->unsigned()->nullable()->default(0)->comment('开始时间');
+            $table->integer('end_time')->unsigned()->nullable()->default(0)->comment('过期时间');
+            $table->integer('checked_time')->unsigned()->nullable()->default(0)->comment('审核时间');
+            $table->primary('push_id');
+            $table->index('end_time');
+            $table->index('status');
         });
     }
 
@@ -78,4 +78,3 @@ class PwDesignPushTable extends Migration
         Schema::dropIfExists('pw_design_push');
     }
 }
-
