2d8e39a476d2c442ecba645db55bddd7d8017864
Seven <shiweidu@outlook.com>
2017-05-29 08:29:21 +0800

    增加 common_config seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // phpwind seeder.
+        $this->call(Wind9Seeder::class);
     }
 }
---
+++ database/seeds/Wind9Seeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Medz\Wind\Models\PwCommonConfig;
+
+class Wind9Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->seedCommonConfigTable();
+    }
+
+    /**
+     * Seed to common_config
+     *
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    protected function seedCommonConfigTable()
+    {
+        $rows = $this->container->make('files')->getRequire(__DIR__.'/phpwind9/pw_common_config.php');
+        foreach ($rows as $item) {
+            PwCommonConfig::create([
+                'name' => $item[0],
+                'namespace' => $item[1],
+                'value' => $item[2],
+                'vtype' => $item[3],
+                'description' => $item[4],
+            ]);
+        }
+    }
+}
