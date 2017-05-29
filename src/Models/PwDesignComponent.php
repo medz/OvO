90d5aadfc6e6b7a27c27e0151d7d7c996eb3a5fc
Seven <shiweidu@outlook.com>
2017-05-29 09:11:55 +0800

    添加模型。

---
+++ src/Models/PwDesignComponent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Medz\Wind\Models;
+
+use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Builder;
+
+class PwDesignComponent extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'pw_design_component';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'comp_id';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
---
+++ src/Models/PwDesignPage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Medz\Wind\Models;
+
+use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Builder;
+
+class PwDesignPage extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'pw_design_page';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'page_id';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
