e0ca3800d559efdd8b8074721e231a8151446654
lkddi <lkddi@163.com>
2017-06-02 13:22:46 +0800

    添加 pw_space 迁移 #132

---
+++ public/php.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ *
+ * @Author: 流星
+ * @Mail: lkddi@13.com
+ * @Date:   2017-06-02 10:32:28
+ * @Last Modified by:   lkddi
+ * @Last Modified time: 2017-06-02 10:33:20
+ */
+
+phpinfo();
+?>
