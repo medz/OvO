e3eb7e61f45b3a689c19b2988edbb5b463e73750
Seven <shiweidu@outlook.com>
2017-06-08 16:53:35 +0800

    Revert "添加 pw_space 迁移 #132"
    This reverts commit e0ca3800d559efdd8b8074721e231a8151446654.

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Medz\Wind\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        //
     }
 
     /**
