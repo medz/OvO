a79fa006903d855cf6babcfc9fc1f6c3cee52c94
Seven <shiweidu@outlook.com>
2017-06-17 00:24:45 +0800

    更新首页头文件信息～

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="mask-icon" href="{{ asset('/fans.svg') }}" color="#000000">
+        <link rel="icon" type="image/x-icon" href="{{ asset('/favicon.ico') }}">
+
         <title>phpwind Fans</title>
 
         <!-- Fonts -->
