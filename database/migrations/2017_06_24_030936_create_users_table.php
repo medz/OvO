0469cdb7cd17db32ae0db1d1eded93dcf2c3fc14
Seven <shiweidu@outlook.com>
2017-06-24 11:21:04 +0800

    增加用户表迁移

---
+++ database/migrations/2017_06_24_030936_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id')->comment('用户ID');
+            $table->string('name')->nullable()->default(null)->comment('用户名');
+            $table->string('email')->nullable()->default(null)->comment('邮箱');
+            $table->string('phone')->nullable()->default(null)->comment('手机');
+            $table->string('password')->comment('密码');
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->unique('name');
+            $table->uniqid('email');
+            $table->uniqid('phone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
