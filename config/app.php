5e116b61c7d892580ffa6f03789567feb34cf88c
Seven <shiweidu@outlook.com>
2017-06-24 11:44:46 +0800

    添加 Oauth 2 认证 server.

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
 
         /*
          * Application Service Providers...
