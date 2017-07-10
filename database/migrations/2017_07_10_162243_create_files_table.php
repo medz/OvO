e22dca55193a6e6deb7e1dd63a4f24728191f22b
Seven <shiweidu@outlook.com>
2017-07-11 00:39:15 +0800

    完成文件类。

--- database/migrations/2017_07_10_162243_create_files_table.php
+++ database/migrations/2017_07_10_162243_create_files_table.php
@@ -15,6 +15,10 @@ class CreateFilesTable extends Migration
     {
         Schema::create('files', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('md5', 100)->comment('文件 md5 值');
+            $table->string('origin_filename')->comment('文件原始名称');
+            $table->string('filename')->comment('文件名');
+            $table->string('mime', 100)->comment('mime type');
             $table->timestamps();
         });
     }
