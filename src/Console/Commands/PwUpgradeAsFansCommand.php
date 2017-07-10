d6dba618380fe1932f556355fb6778ef0a88a187
Seven <shiweidu@outlook.com>
2017-07-11 00:57:14 +0800

    增加 PW or Fans 1.0 to Fans 1.1

---
+++ src/Console/Commands/PwUpgradeAsFansCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Medz\Fans\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class PwUpgradeAsFansCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'upgrade:pw';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+    }
+}
