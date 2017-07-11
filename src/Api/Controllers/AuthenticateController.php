5ea743c0e4c7414d87e0d6537590aa710c23f42b
Seven <shiweidu@outlook.com>
2017-07-11 14:53:46 +0800

    开发完成部分获取 token 数据。

--- src/Api/Controllers/AuthenticateController.php
+++ src/Api/Controllers/AuthenticateController.php
@@ -3,11 +3,16 @@
 namespace Medz\Fans\Api\Controllers;
 
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\JWTAuth;
 
 class AuthenticateController extends Controller
 {
-    public function authenticate(Request $request)
+    public function authenticate(Request $request, JWTAuth $auth)
     {
-        dd($request->all());
+        $credentials = $request->only(['email', 'password']);
+
+        dd(
+            $auth->attempt($credentials)
+        );
     }
 }
