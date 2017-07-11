4ed3f0240462ffb8c5a6162505325f8f5eb9c679
Seven <shiweidu@outlook.com>
2017-07-11 15:21:35 +0800

    完成用户认证API开发.

--- src/Api/Controllers/AuthenticateController.php
+++ src/Api/Controllers/AuthenticateController.php
@@ -10,9 +10,13 @@ class AuthenticateController extends Controller
     public function authenticate(Request $request, JWTAuth $auth)
     {
         $credentials = $request->only(['email', 'password']);
+        $token = $auth->attempt($credentials); 
 
-        dd(
-            $auth->attempt($credentials)
-        );
+        return $token = $auth->attempt($credentials) !== false
+            ? $this->response()->array([
+                'token' => $token,
+                'user' => $request->user(),
+            ])->setStatusCode(201)
+            : $this->response()->errorInternal('No token created.');
     }
 }
--- src/Api/Controllers/Controller.php
+++ src/Api/Controllers/Controller.php
@@ -5,9 +5,10 @@ namespace Medz\Fans\Api\Controllers;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+// use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Dingo\Api\Routing\Helpers;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use DispatchesJobs, ValidatesRequests, Helpers;
 }
