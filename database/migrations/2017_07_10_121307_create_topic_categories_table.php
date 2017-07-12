b7f05847981d44017927ea4f3513fc76e756573e
Seven <shiweidu@outlook.com>
2017-07-13 00:46:56 +0800

    完善分类数据模型

--- database/migrations/2017_07_10_121307_create_topic_categories_table.php
+++ database/migrations/2017_07_10_121307_create_topic_categories_table.php
@@ -18,6 +18,10 @@ class CreateTopicCategoriesTable extends Migration
             $table->string('name', 150)->comment('类别名称');
             $table->integer('icon')->nullable()->default(null)->comment('icon');
             $table->string('bg_color', 50)->nullable()->default(null)->comment('背景颜色');
+            $table->integer('topic_count')->unsigned()->nullable()->default(0)->comment('话题统计');
+            $table->integer('post_count')->unsigned()->nullable()->default(0)->comment('讨论统计');
+            $table->integer('parent')->unsigned()->nullable()->default(null)->comment('父级类别');
+            $table->tinyInteger('new')->unsigned()->nullable()->default(1)->comment('是否允许发布话题');
             $table->timestamps();
             $table->softDeletes();
         });
