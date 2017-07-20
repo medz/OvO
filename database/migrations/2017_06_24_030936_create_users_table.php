c7bb3e76e57c3a022900e27716c9ff35fbaf5a86
Seven Du <shiweidu@outlook.com>
2017-07-20 21:50:24 +0800

    更新用户资料表设计。

--- database/migrations/2017_06_24_030936_create_users_table.php
+++ database/migrations/2017_06_24_030936_create_users_table.php
@@ -15,13 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id')->comment('用户ID');
-            $table->string('name')->nullable()->default(null)->comment('用户名');
-            $table->string('email')->nullable()->default(null)->comment('邮箱');
-            $table->string('phone')->nullable()->default(null)->comment('手机');
+            $table->string('login', 150)->nullable()->default(null)->comment('登录名');
+            $table->string('email', 150)->nullable()->default(null)->comment('邮箱');
+            $table->string('phone', 100)->nullable()->default(null)->comment('手机');
+            $table->string('name')->nullable()->default(null)->comment('名字');
+            $table->string('location')->nullable()->default(null)->comment('位置');
+            $table->string('bio')->nullable()->default(null);
+            $table->string('gender')->nullable()->default(null);
+
             $table->string('password')->comment('密码');
             $table->string('pw_password', 100)->nullable()->default(null)->comment('pw 老用户密码');
             $table->string('pw_salt', 100)->nullable()->default(null)->comment('pw 用户密码计算盐值');
-            $table->integer('pw_id')->unsigned()->nullable()->default(0)->comment('pw 原始用户ID');
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
