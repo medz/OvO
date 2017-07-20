f27d3c7c7ad084d3c735ac0e110fc953bf544248
Seven Du <shiweidu@outlook.com>
2017-07-20 23:05:02 +0800

    完成角色许可表。

--- database/migrations/2017_07_20_145742_create_permissions_table.php
+++ database/migrations/2017_07_20_145742_create_permissions_table.php
@@ -15,7 +15,12 @@ class CreatePermissionsTable extends Migration
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name', 150);
+            $table->string('display_name')->nullable();
+            $table->string('description')->nullable();
             $table->timestamps();
+
+            $table->unique('name');
         });
     }
 
