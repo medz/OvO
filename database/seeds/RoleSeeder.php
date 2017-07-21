ad3bbb00e251718d307d8afeb7f69ae60493adaa
Seven Du <shiweidu@outlook.com>
2017-07-21 09:32:09 +0800

    增加角色填充。

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class RoleSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RoleSeeder::class);
+        //
     }
 }
