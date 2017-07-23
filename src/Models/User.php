f4f32247ed0e13777fc57f8645088b21e265975d
Seven Du <shiweidu@outlook.com>
2017-07-23 22:10:04 +0800

    增加用户数据隐藏。

--- src/Models/User.php
+++ src/Models/User.php
@@ -9,6 +9,19 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = ['email', 'phone', 'password' 'pw_password', 'pw_salt'];
+
+    /**
+     * Get auth password.
+     *
+     * @return string
+     * @author Seven Du <shiweidu@outlook.com>
+     */
     public function getAuthPassword()
     {
         // dd(request('password'));
