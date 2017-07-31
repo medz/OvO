47b6f9c4770104688e0aabcaf84e44de9b943367
Seven Du <shiweidu@outlook.com>
2017-07-31 11:10:28 +0800

    优化登录健，支持 email/login/phone/id 登录。

--- src/Api/Controllers/Auth/LoginController.php
+++ src/Api/Controllers/Auth/LoginController.php
@@ -42,7 +42,7 @@ class LoginController extends Controller
         ];
 
         return ! ($token = $this->auth->attempt($credentials))
-            ? $this->response()->error('登录失败', 422)
+            ? $this->response()->error(trans('auth.failed'), 422)
             : $this->response()->array([
                 'token' => $token,
                 'user' => array_merge(($user = $request->user())->toArray(), [
@@ -67,7 +67,7 @@ class LoginController extends Controller
         $map = [
             'email' => filter_var($login, FILTER_VALIDATE_EMAIL),
             'phone' => preg_match('/^(\+?0?86\-?)?((13\d|14[57]|15[^4,\D]|17[3678]|18\d)\d{8}|170[059]\d{7})$/', $login),
-            'name' => preg_match('/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/', $login),
+            'login' => preg_match('/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/', $login),
         ];
 
         foreach ($map as $field => $value) {
