88af516cae413b30a1ed4f239a9cfb68f800b0fb
Seven Du <shiweidu@outlook.com>
2017-09-24 12:12:54 +0800

    增加用户权限与角色关联中间表。

---
+++ database/migrations/2017_09_24_041205_create_ability_role_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAbilityRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ability_role', function (Blueprint $table) {
+            $table->integer('ability_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+
+            $table->primary(['ability_id', 'role_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ability_role');
+    }
+}
