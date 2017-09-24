8fef7acb34f5f7e444044740ad334e0d0e6235ae
Seven Du <shiweidu@outlook.com>
2017-09-24 14:08:28 +0800

    删除废弃的工具。

--- app/Api/Controllers/Controller.php
+++ app/Api/Controllers/Controller.php
@@ -2,7 +2,6 @@
 
 namespace App\Api\Controllers;
 
-use Dingo\Api\Routing\Helpers;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 // use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -10,5 +9,5 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class Controller extends BaseController
 {
-    use DispatchesJobs, ValidatesRequests, Helpers;
+    use DispatchesJobs, ValidatesRequests;
 }
