c24a6d7f246ee0d2f0b84c4577592ef07bc5c918
Seven Du <shiweidu@outlook.com>
2017-12-19 11:17:29 +0800

    移除

--- app/Application.php
+++ app/Application.php
@@ -2,15 +2,14 @@
 
 namespace App;
 
-use App\Contracts\Applicable;
 use Illuminate\Foundation\Application as LaravelApplication;
 
-class Application extends LaravelApplication implements Applicable
+class Application extends LaravelApplication
 {
     /**
      * Defined the application version.
      */
-    const VERSION = '1.1.0-alpha';
+    const VERSION = '2.0.0';
 
     /**
      * Register the core class aliases in the container.
@@ -24,21 +23,5 @@ class Application extends LaravelApplication implements Applicable
 
         // Register class aliases.
         $this->alias('app', static::class);
-    }
-
-    /**
-     * Create an HTTP response that represents the object.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return mixed
-     * @author Seven Du <shiweidu@outlook.com>
-     */
-    public function toResponse($request)
-    {
-        if ($request->path() !== '/') {
-            return $this->abort(404);
-        }
-
-        return view('welcome');
     }
 }
