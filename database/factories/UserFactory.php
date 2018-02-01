3e441f333db2cb93befe91819c80a55c054f5b45
Seven Du <shiweidu@outlook.com>
2018-02-01 13:18:19 +0800

    chore: Add user model factory
    Co-Authored-By: Seven Du <shiweidu@outlook.com>

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,11 +13,9 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Models\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'login' => $faker->name,
+        'password' => bcrypt('password'),
     ];
 });
