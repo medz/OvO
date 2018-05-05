bfd51d304e3f1ae37c539c90a415f8d910e09552
Seven Du <shiweidu@outlook.com>
2018-05-05 09:35:06 +0800

    chore: Application add Laravel version method

--- app/Application.php
+++ app/Application.php
@@ -12,6 +12,17 @@ class Application extends LaravelApplication
     const VERSION = '2.0.0';
 
     /**
+     * Get the version number of the Laravel framework.
+     * 
+     * @return string
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function laravelVersion(): string
+    {
+        return parent::VERSION;
+    }
+
+    /**
      * Register the core class aliases in the container.
      *
      * @return void
