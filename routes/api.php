a6ada3c78ab7e4db8daed0fc297fa947971db60f
Seven Du <shiweidu@outlook.com>
2018-05-05 09:31:33 +0800

    chore: 修改命名空间别名

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Api\Controllers;
+use App\Api\Controllers as API;
 use App\Api\Middleware\CrossDomain;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Contracts\Routing\Registrar as RouteContract;
@@ -18,28 +18,28 @@ use Illuminate\Contracts\Routing\Registrar as RouteContract;
 
 Route::group(['prefix' => 'v1', 'middleware' => CrossDomain::class], function (RouteContract $api) {
     $api->group(['prefix' => 'auth'], function (RouteContract $api) {
-        $api->post('login', Controllers\AuthController::class.'@login');
-        $api->any('logout', Controllers\AuthController::class.'@logout');
-        $api->any('refresh', Controllers\AuthController::class.'@refresh');
-        $api->get('me', Controllers\AuthController::class.'@me');
+        $api->post('login', API\AuthController::class.'@login');
+        $api->any('logout', API\AuthController::class.'@logout');
+        $api->any('refresh', API\AuthController::class.'@refresh');
+        $api->get('me', API\AuthController::class.'@me');
     });
-    $api->get('user', Controllers\AuthController::class.'@me');
-    $api->post('register', Controllers\UserRegisterController::class);
+    $api->get('user', API\AuthController::class.'@me');
+    $api->post('register', API\UserRegisterController::class);
     $api->group(['prefix' => 'users'], function (RouteContract $api) {
-        $api->get('', Controllers\User\UserController::class.'@index');
-        $api->get('{user}', Controllers\User\UserController::class.'@show');
+        $api->get('', API\User\UserController::class.'@index');
+        $api->get('{user}', API\User\UserController::class.'@show');
     });
     $api->group(['prefix' => 'tags'], function (RouteContract $api) {
-        $api->get('', Controllers\TagController::class.'@index');
-        $api->get('{tag}', Controllers\TagController::class.'@show');
+        $api->get('', API\TagController::class.'@index');
+        $api->get('{tag}', API\TagController::class.'@show');
     });
 
-    $api->get('forums', Controllers\Forum\ForumController::class.'@index');
-    $api->get('forums/{forum}', Controllers\Forum\ForumController::class.'@show');
-    $api->get('forums/{forum}/categories', Controllers\Forum\CategoryController::class.'@index');
-    $api->get('forum->categories/{category}', Controllers\Forum\CategoryController::class.'@show');
-    $api->get('forums/{forum}/topics', Controllers\Forum\TopicController::class.'@index');
-    $api->post('forums/{forum}/topics', Controllers\Forum\TopicController::class.'@store');
-    $api->get('forum->topics', Controllers\Forum\TopicController::class.'@all');
-    $api->get('forum->topics/{topic}', Controllers\Forum\TopicController::class.'@show');
+    $api->get('forums', API\Forum\ForumController::class.'@index');
+    $api->get('forums/{forum}', API\Forum\ForumController::class.'@show');
+    $api->get('forums/{forum}/categories', API\Forum\CategoryController::class.'@index');
+    $api->get('forum->categories/{category}', API\Forum\CategoryController::class.'@show');
+    $api->get('forums/{forum}/topics', API\Forum\TopicController::class.'@index');
+    $api->post('forums/{forum}/topics', API\Forum\TopicController::class.'@store');
+    $api->get('forum->topics', API\Forum\TopicController::class.'@all');
+    $api->get('forum->topics/{topic}', API\Forum\TopicController::class.'@show');
 });
