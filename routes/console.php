79562c777a86612f7d0c059f37eeb8e1613d7808
Seven Du <shiweidu@outlook.com>
2019-01-14 09:26:50 +0800

    feat: Console add create founder command

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,18 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('db:create:founder', function () {
+    $countries = \App\Models\InternationalTelephoneCode::get()->pluck('name', 'code');
+    $ttc = $this->choice('Select country', $countries->all());
+    $phone = $this->ask('Enter your mobile number');
+    $user = new \App\Models\User([
+        'international_telephone_code' => $ttc,
+        'phone' => $phone,
+    ]);
+    $user->save();
+    \App\Models\Jurisdiction::nodes()->each(function (string $node) use ($user) {
+        $user->jurisdictions()->create(['node' => $node]);
+    });
+    $this->comment(sprintf('Created a User:%s success.', $user->id));
+})->describe('Create a founder');
