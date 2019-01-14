bc2ad6b108a4a95f7c4a6d207981771072c4ea10
Seven Du <shiweidu@outlook.com>
2019-01-14 12:07:11 +0800

    chore: add TTC manage info

--- resources/lang/en/jurisdiction.php
+++ resources/lang/en/jurisdiction.php
@@ -5,6 +5,10 @@ return [
         'name' => 'Talk > Desrory',
         'desc' => 'Destory Talk permission.',
     ],
+    'ttc:manage' => [
+        'name' => 'TTC > Manage',
+        'desc' => 'Manage International Telephone Codes',
+    ],
     'user:jurisdiction' => [
         'name' => 'User > Jurisdiction',
         'desc' => 'Change User Jurisdiction Nodes',
