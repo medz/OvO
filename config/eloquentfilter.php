2b7c197e570b0452565a92112983a6525b0837bd
Seven Du <shiweidu@outlook.com>
2020-03-15 05:56:36 +0000

    Apply fixes from StyleCI

--- config/eloquentfilter.php
+++ config/eloquentfilter.php
@@ -22,7 +22,7 @@ return [
     | you can enter the path to your own at this point
     |
     */
-//    'generator' => [
-//        'stub' => app_path('stubs/modelfilter.stub')
-//    ]
+    //    'generator' => [
+    //        'stub' => app_path('stubs/modelfilter.stub')
+    //    ]
 ];
